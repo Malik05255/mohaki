2fde4280f58668bf1559f85cd633abf3587a2548
Malik05255 <mmbbrr99@gmail.com>
2026-09-16 02:09:34 +0300

    Add VM controller interface

---
+++ host/src/VmController.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <windows.h>
+#include <filesystem>
+#include <optional>
+#include <string>
+
+namespace jawal {
+
+struct VmConfig {
+    std::filesystem::path qemuExe;
+    std::filesystem::path runtimeDir;
+    std::filesystem::path systemDisk;
+    std::filesystem::path userDisk;
+    std::filesystem::path snapshotDisk;
+    unsigned memoryMb{4096};
+    unsigned cpuCores{4};
+};
+
+class VmController final {
+public:
+    VmController() = default;
+    ~VmController();
+
+    VmController(const VmController&) = delete;
+    VmController& operator=(const VmController&) = delete;
+
+    bool Start(HWND renderParent, const VmConfig& config, std::wstring* error);
+    void Stop() noexcept;
+    bool Running() const noexcept;
+
+private:
+    std::wstring BuildCommandLine(HWND renderParent, const VmConfig& config) const;
+
+    PROCESS_INFORMATION process_{};
+};
+
+} // namespace jawal
